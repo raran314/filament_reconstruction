294ef36408a068227aedf8e27727d17095151a62
raran314 <123431219+raran314@users.noreply.github.com>
2024-05-30 12:37:54 -0400

    Update import_swc_data.py

--- import_swc_data.py
+++ import_swc_data.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import networkx as nx
 from matplotlib import pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D  # Ensure 3D plotting is imported
 
 def swc_to_graph(swc):
     """
@@ -30,22 +31,35 @@ for cc in conncomp:
     curves.append(curve)
 
 def sort_curve(rr):
-    centroid = np.mean(rr,axis=0)
+    centroid = np.mean(rr, axis=0)
     rr_centered = rr - centroid        
-    _,_, V = np.linalg.svd(rr_centered, full_matrices=False)
-    v1 = V[0,:]
+    _, _, V = np.linalg.svd(rr_centered, full_matrices=False)
+    v1 = V[0, :]
     orientation = v1 * np.sign(np.sum(v1 * (rr_centered[-1, :] - rr_centered[0, :])))
     slist = np.dot((rr - centroid), orientation)
     sorted_indices = np.argsort(slist)
     return centroid + rr_centered[sorted_indices]
 
-fig=plt.figure()
+fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
-# for i in np.random.choice(len(curves), 1000):
 for i in range(len(curves)):
     curve = curves[i]
     curve = sort_curve(curve)
-    ax.plot(curve[:,0], curve[:,1], curve[:,2],'-',linewidth=0.7,markersize=2)    
-ax.axis('equal')
+    ax.plot(curve[:, 0], curve[:, 1], curve[:, 2], '-', linewidth=0.7, markersize=2)
 
+# Manually set equal aspect ratio
+x_limits = ax.get_xlim3d()
+y_limits = ax.get_ylim3d()
+z_limits = ax.get_zlim3d()
 
+x_range = x_limits[1] - x_limits[0]
+y_range = y_limits[1] - y_limits[0]
+z_range = z_limits[1] - z_limits[0]
+
+plot_radius = 0.5 * max([x_range, y_range, z_range])
+
+ax.set_xlim3d([np.mean(x_limits) - plot_radius, np.mean(x_limits) + plot_radius])
+ax.set_ylim3d([np.mean(y_limits) - plot_radius, np.mean(y_limits) + plot_radius])
+ax.set_zlim3d([np.mean(z_limits) - plot_radius, np.mean(z_limits) + plot_radius])
+
+plt.show()
